478585fda0eb8f1adb2e5901aca6b906e49c0d77
dontuseitiguess <riten681@icloud.com>
2025-05-17 15:39:27 +0200

    Update db.php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $dbname = 'fable_db';
 $username = 'root';
 $password = '';
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
